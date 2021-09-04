f3f92d459afcc51ffb27ddd9faa040e699847795
Dipankar Malick <60977135+Dipankar97@users.noreply.github.com>
2021-09-04 11:49:55 +0530

    Update guessing_Number_Game.py

--- guessing_Number_Game.py
+++ guessing_Number_Game.py
@@ -1,16 +1,21 @@
 import random
-num=0
-myNum=random.randint(0,101)
 
-while(num>=0):
-    num=int(input("Guess the number(0-100): "))
+def main():
+    num=0
+    myNum=random.randint(0,101)
+    
+    while(num>=0):
+        num=int(input("Guess the number(0-100): "))
+    
+        if(num == myNum):
+            print("WOOOOO.....HOOOOOO.....\nYou Guess the correct number. . .")
+            break
+        elif(num>myNum):
+            print("Number is too large! ! !")
+        else:
+            print("Number is too small! ! !")
+    
+    print("My number is {}\nGame Finish. . . .".format(myNum))
 
-    if(num == myNum):
-        print("WOOOOO.....HOOOOOO.....\nYou Guess the correct number. . .")
-        break
-    elif(num>myNum):
-        print("Number is too large! ! !")
-    else:
-        print("Number is too small! ! !")
-
-print("My number is {}\nGame Finish. . . .".format(myNum))
+if __name__ == "__main__":
+    main()
